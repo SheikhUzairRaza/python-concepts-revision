c794053395cf18482585850c840a28bde8bf5dbf
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2025-01-11 00:40:45 +0500

    Add basic Python print examples and explanations

---
+++ Basics/hello.py
@@ -0,0 +1,19 @@
+print("Hello")
+print("World")
+
+# The first point to note: The `print` function prints each statement on a separate line by default.
+
+print('Single quotes are allowed for strings.')
+print("Double quotes are also allowed for strings.")
+
+# If you want to print two `print` statements on the same line, 
+# you can use the `end` parameter to avoid the default new line.
+
+print("Hello", end=" ")
+print("World")
+
+# Practice question:
+
+print("My name is Uzair", end=" ")
+print("My age is 20", end=" ")
+print("I am revising my Python programming concepts")
