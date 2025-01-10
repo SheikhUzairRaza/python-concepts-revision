409ae787dc82e25cfeb8180e61c5a21804b80723
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2025-01-11 00:54:46 +0500

    Add explanation and examples of single-line and multi-line comments in Python

---
+++ Basics/comments.py
@@ -0,0 +1,27 @@
+# In computer programming, comments are used to provide hints, explanations, or notes within the code.  
+# They help make the code more understandable and maintainable for developers.  
+
+# Comments are completely ignored by the interpreter and have no effect on the program's execution.  
+
+# Types of Comments in Python:
+# 1. Single-line Comments
+# 2. Multi-line Comments
+
+print("I am revising the concept of comments.")
+
+# Example of a single-line comment:
+# The following lines of code are commented out and won't be executed.
+# print("Hello")
+# print("World")
+# print("2")
+
+# Example of multi-line comments:
+"""
+This is an example of a multi-line comment, also referred to as a docstring. 
+Docstrings are commonly used for documentation and will be explored further in upcoming lessons.
+"""
+
+'''
+This is another way to write multi-line comments. 
+However, the standard and preferred way to write multi-line comments is by using triple double quotes (""").
+'''
