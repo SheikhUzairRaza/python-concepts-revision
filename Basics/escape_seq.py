376a4508898542153a7110935e1fd658e87acf3d
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2025-01-12 00:37:14 +0500

    Add explanation and examples of escape characters in Python

---
+++ Basics/escape_seq.py
@@ -0,0 +1,24 @@
+# Escape characters or sequences are special characters in Python that enable certain behaviors, 
+# such as formatting the output. They never appear as part of the printed output but alter how 
+# the output is displayed.
+
+# Common Escape Characters:
+# \'  -> Single quotation
+# \"  -> Double quotation
+# \\  -> Backslash
+# \n  -> New line
+# \t  -> Tab
+# \r  -> Carriage return
+# \b  -> Backspace
+
+# Example: Using \n to print a new line in the output.
+print("Hello world\nI am revising Python concepts")
+
+# Escape sequences do not appear in the output but perform specific tasks.
+
+# Practice Questions:
+print("My name is Uzair\nMy age is 20\nMy gender is Male")  # Using \n for new lines
+print("My name is Uzair\tMy age is 20")                    # Using \t for tabs
+print("My name is \"Sheikh Uzair Raza\"")                  # Using \" for double quotes
+print("My name is \'Sheikh Uzair Raza\'")                  # Using \' for single quotes
+print("My name is Sheikh Uzair Raza a\nd My age is 20")    # Using \n for a new line
