0e7bc9905e6c39178a2be5c676fecea36c6602bf
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2025-01-12 13:30:05 +0500

    Print Hello World 10 times through while loop

---
+++ Loops/while.py
@@ -0,0 +1,7 @@
+# looping means repeating something over and over until a particular condition is satisfied
+
+times = 0
+while times < 10:
+    print("Hello World")
+    times += 1
+    print("Ok bye")
