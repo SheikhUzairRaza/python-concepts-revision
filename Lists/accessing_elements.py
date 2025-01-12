45d6c81ea39fefed312002da633612781fbcaca4
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2025-01-12 14:49:33 +0500

    Add example of list element access using indexing in Python

---
+++ Lists/accessing_elements.py
@@ -0,0 +1,15 @@
+# Example: Accessing elements in a Python list
+
+# Defining a list with heterogeneous data
+a = [55, 43, "SUR", True]
+
+# Printing the entire list
+print(a)
+
+# Accessing the first element of the list
+print(a[0])  # Output: 55 (first element)
+print(type(a[0]))  # Checking the type of the first element
+
+# Accessing the last element of the list using negative indexing
+print(a[-1])  # Output: True (last element)
+print(type(a[-1]))  # Checking the type of the last element
